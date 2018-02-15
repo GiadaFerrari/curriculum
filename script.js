56b2a09dc260f27de865871cf075455fe9517a02
Giada Ferrari <giadaferrari@laptop-m4g315ot.kea.local>
2018-02-15 14:34:20 +0100

    clickable logo

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let logo = document.querySelector(".logo");
+
+logo.addEventListener('click',()=>{
+    window.location.replace('index.html')
+})
+
+
+
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
 }
