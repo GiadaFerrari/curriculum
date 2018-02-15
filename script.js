6763d90664166889e8a369daa18461ab8b879533
decka <decka@github.com>
2018-02-15 12:03:34 +0100

    nav media queries

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
- function load(){
-    document.getElementById("sidenav").style.width = "250px";
+
+
+function load(){
+    //document.getElementById("sidenav").style.width = "250px";
 
 }
 
 function openNav() {
      document.getElementById("sidenav").style.transition = "0.5s";
-    document.getElementById("sidenav").style.width = "250px";
+    //document.getElementById("sidenav").style.width = "250px";
 
 }
 
