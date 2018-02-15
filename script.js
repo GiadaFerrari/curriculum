b8351ede0079c6ce479b1e8649d6cfa2223a2a81
decka <decka@github.com>
2018-02-15 11:32:44 +0100

    navigation

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function openNav() {
+ function load(){
     document.getElementById("sidenav").style.width = "250px";
+
+}
+
+function openNav() {
+     document.getElementById("sidenav").style.transition = "0.5s";
+    document.getElementById("sidenav").style.width = "250px";
+
 }
 
 function closeNav() {
-
+    document.getElementById("sidenav").style.transition = "0.5s";
     document.getElementById("sidenav").style.width = "0";
 
 }
 
+window.onload = load
