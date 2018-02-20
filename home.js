8f5e7325e9a7e73fcbe64d65d0409343bf731780
Giada Ferrari <giadaferrari@laptop-7gv25l1h.kea.local>
2018-02-20 12:54:16 +0100

    core areas json fixed, empty arrays

--- home.js
+++ home.js
@@ -34,7 +34,7 @@ function closeNav() {
 //dynamic
 
 const template = document.querySelector('template').content;
-const source = 'coreAreas.json'
+const source = 'coreAreas.json';
 
 fetch(source).then(result => result.json()).then(data => showData(data));
 
@@ -46,15 +46,37 @@ function showData(e) {
 
         const clone = template.cloneNode(true);
         const appendHere = document.querySelector('.coreAreasDes');
+        const appendKnowl = clone.querySelector('.knowledge')
+        const appendSkills = clone.querySelector('.skills')
+        const appendCompet = clone.querySelector('.competencies')
+
 
 
         clone.querySelector('.CAContainer a').id = elem.id;
-       // clone.querySelector('.CAContainer').id = elem.id;
+        // clone.querySelector('.CAContainer').id = elem.id;
         clone.querySelector('.name').textContent = elem.name;
         clone.querySelector('.ects').textContent = elem.ects;
-        clone.querySelector('.knowledge').textContent = elem.knowledge;
-        clone.querySelector('.skills').textContent = elem.skills;
-        clone.querySelector('.competencies').textContent = elem.competencies;
+        elem.knowledge.forEach(know => {
+            let knoLi = document.createElement('li')
+
+            knoLi.textContent = know;
+            appendKnowl.appendChild(knoLi)
+
+        })
+        elem.skills.forEach(skill => {
+            let skillsLi = document.createElement('li')
+
+            skillsLi.textContent = skill;
+            appendSkills.appendChild(skillsLi)
+
+        })
+        elem.competencies.forEach(com => {
+            let comLi = document.createElement('li')
+
+            comLi.textContent = com;
+            appendCompet.appendChild(comLi)
+
+        })
 
         appendHere.appendChild(clone);
         console.log(elem.knowledge)
@@ -66,16 +88,22 @@ function showData(e) {
 //core areas slider
 let coreArea = document.querySelector('.circle');
 
-window.onscroll = function(){sliderMobile()}
+window.onscroll = function () {
+    sliderMobile()
+}
+
 function sliderMobile() {
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop>600){
-        console.log ('hi papa');
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        console.log('hi papa');
         coreArea.classList.add('moveMeUpCA', 'gridMe');
         coreArea.classList.remove('coreAreas')
 
-    } else { console.log ('hi mama');
-            coreArea.classList.add('coreAreas')
-     coreArea.classList.remove('moveMeUpCA', 'gridMe')}}
+    } else {
+        console.log('hi mama');
+        coreArea.classList.add('coreAreas')
+        coreArea.classList.remove('moveMeUpCA', 'gridMe')
+    }
+}
 
 
 let CAAnchor = document.querySelectorAll('.circle a');
@@ -86,19 +114,17 @@ CAAnchor.forEach((e) => {
     e.addEventListener('click', padMe);
 
 
-        function padMe(e) {
+    function padMe(e) {
 
-            CATarget.forEach((a) => {
-                a.classList.remove('padMe')
-            })
-            let link = this.getAttribute("href");
-            let cont = document.querySelector(link);
-            console.log(cont)
+        CATarget.forEach((a) => {
+            a.classList.remove('padMe')
+        })
+        let link = this.getAttribute("href");
+        let cont = document.querySelector(link);
+        console.log(cont)
 
-            cont.classList.add('padMe');
+        cont.classList.add('padMe');
 
 
-        }
     }
-)
-
+})
