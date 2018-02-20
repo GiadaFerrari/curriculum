8fbef1cb7ba9d6f5981b659e72a5c6c0ed73694c
Giada Ferrari <giadaferrari@laptop-7gv25l1h.kea.local>
2018-02-20 14:52:57 +0100

    core areas grid

--- home.js
+++ home.js
@@ -93,7 +93,7 @@ window.onscroll = function () {
 }
 
 function sliderMobile() {
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
         console.log('hi papa');
         coreArea.classList.add('moveMeUpCA', 'gridMe');
         coreArea.classList.remove('coreAreas')
